bfffa7b4d9b9e282da03830db462091ce23e0116
Aakash Gupta <54316679+Ablaze3883@users.noreply.github.com>
2022-06-11 20:02:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
-const shoot = (x) => {};
+const n = 10;
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -31,120 +31,49 @@ const style = {
 };
 
 export default function BasicGrid() {
-
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
   const [showColor, setShowColor] = React.useState(false);
+  const numbers = ['1', '2', '3', '4', '5'];
+  const ABC = [];
+  for (const [i, n] of numbers.entries()) {
+    ABC.push(
+      <Item>
+        <Button
+          style={
+            showColor
+              ? { backgroundColor: "green" }
+              : { backgroundColor: "red" }
+          }
+          onClick={handleOpen}
+        >
+          {numbers}
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <label for="myfile">Select a file:</label>
+            <input
+              type="file"
+              accept="image/*,.jpeg,.png,.svg,.pdf, .mp4"
+              onChange={() => setShowColor(true)}
+            />
+          </Box>
+        </Modal>
+      </Item>
+    );
+  }
 
   return (
     <Box sx={{ flexGrow: 1 }} className="App">
       <Grid container spacing={2}>
         <Grid item xs={3}>
-          <Item>
-            <Button
-              style={
-                showColor
-                  ? { backgroundColor: "green" }
-                  : { backgroundColor: "red" }
-              }
-              onClick={(handleOpen)}
-            >
-              11
-            </Button>
-            <Modal
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={style}>
-              <label for="myfile">Select a file:</label>
-              <input
-                type="file"
-                accept="image/*,.jpeg,.png,.svg,.pdf, .mp4"
-                onChange={() => setShowColor(true)}
-              />
-
-              </Box>
-            </Modal>
-           </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(12)}>12</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(13)}>13</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(14)}>14</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(21)}>21</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(22)}>22</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(23)}>23</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(24)}>24</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(31)}>31</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(32)}>32</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(33)}>33</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(34)}>34</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(41)}>41</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(42)}>42</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(43)}>43</button>
-          </Item>
-        </Grid>
-        <Grid item xs={3}>
-          <Item>
-            <button onClick={shoot(44)}>44</button>
-          </Item>
+          {ABC}
         </Grid>
       </Grid>
     </Box>
